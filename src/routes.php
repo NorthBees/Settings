1b0348e3a5f20afa22200061153d291748bce39e
sdsmith1981 <steven@sd-smith.co.uk>
2023-06-10 10:40:46 +0100

    Update routes.php

--- src/routes.php
+++ src/routes.php
@@ -2,4 +2,4 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::resource('settings', SettingController::class)->middleware(['auth']);
+Route::resource('settings', \NorthBees\Settings\Http\Controllers\SettingController::class)->middleware(['auth']);
